03e20987022466e14098ec9ad7e1966aa4ccfe01
hinst <hinstance@yandex.ru>
2026-02-28 21:41:09 +0100

    Adding function BuildUrlQueryParams

--- web.go
+++ web.go
@@ -2,7 +2,9 @@ package common
 
 import (
 	"net/http"
+	"net/url"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -14,4 +16,19 @@ type WebFunction func(response http.ResponseWriter, request *http.Request)
 
 func SetCacheAge(response http.ResponseWriter, duration time.Duration) {
 	response.Header().Set(CacheControlHeader, "max-age="+strconv.Itoa(int(duration.Seconds())))
+}
+
+func BuildUrlQueryParams(parameters map[string]string) string {
+	var parts []string
+	var first = true
+	for key, value := range parameters {
+		if first {
+			parts = append(parts, "?")
+			first = false
+		} else {
+			parts = append(parts, "&")
+		}
+		parts = append(parts, url.QueryEscape(key), "=", url.QueryEscape(value))
+	}
+	return strings.Join(parts, "")
 }

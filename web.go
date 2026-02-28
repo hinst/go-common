1d5d21118d71712b2ce9e511177bed9499e67f7a
hinst <hinstance@yandex.ru>
2026-02-28 21:20:25 +0100

    Adding function SetCacheAge

--- web.go
+++ web.go
@@ -1,9 +1,17 @@
 package common
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+	"time"
+)
 
 const ContentTypeHeader = "Content-Type"
 const CacheControlHeader = "Cache-Control"
 const ContentTypeJson = "application/json"
 
 type WebFunction func(response http.ResponseWriter, request *http.Request)
+
+func SetCacheAge(response http.ResponseWriter, duration time.Duration) {
+	response.Header().Set(CacheControlHeader, "max-age="+strconv.Itoa(int(duration.Seconds())))
+}

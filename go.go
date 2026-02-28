921d78cb4f7f1afbcd21fada3746cb111c74bad9
hinst <hinstance@yandex.ru>
2026-02-28 22:03:07 +0100

    Adding functions ioClose and ioCloseSilently

--- go.go
+++ go.go
@@ -1,9 +1,21 @@
 package common
 
+import (
+	"io"
+)
+
 func IfElse[T any](condition bool, ifTrue T, ifFalse T) T {
 	if condition {
 		return ifTrue
 	} else {
 		return ifFalse
 	}
+}
+
+func IoClose(v io.Closer) {
+	AssertError(v.Close())
+}
+
+func IoCloseSilently(v io.Closer) {
+	IgnoreError(v.Close())
 }
